c43392ad6f2a0441375f533640295665751f9958
Ben Reynwar <ben@reynwar.net>
2016-01-22 22:44:46 -0700

    Switched left/right and up/down to be more intuitive.

--- edit_keys.py
+++ edit_keys.py
@@ -4,39 +4,40 @@ from stenoprog.emacs_keys import from_emacs_command
 
 edit_map = {
     # Deletes
-    ((NON, NON), (UPP, NON, UPP, NON)): (True, '{#BackSpace}'),
+    ((NON, NON), (NON, UPP, UPP, NON)): (True, '{#BackSpace}'),
+    ((NON, NON), (NON, LOW, UPP, NON)): (True, C('d')),
     # Delete from this point to end of line (to kill-ring)
-    ((NON, LOW), (LOW, NON, UPP, NON)): (False, C('k')),
+    ((NON, LOW), (NON, LOW, UPP, NON)): (False, C('k')),
     # Delete this line
-    ((NON, NON), (MID, NON, UPP, NON)): (True, from_emacs_command('kill-whole-line')),
+    ((NON, NON), (NON, MID, UPP, NON)): (True, from_emacs_command('kill-whole-line')),
     # Paste from kill-ring
     ((NON, NON), (NON, NON, UPP, NON)): (False, C('y')),
     # Delete previous word
-    ((NON, UPP), (UPP, NON, UPP, NON)): (True, M('Backspace')),
+    ((NON, UPP), (NON, UPP, UPP, NON)): (True, M('Backspace')),
     # Moves
-    ((NON, NON), (UPP, NON, NON, NON)): (True, '{#Left}'),
-    ((NON, NON), (LOW, NON, NON, NON)): (True, '{#Right}'),
-    ((NON, NON), (NON, UPP, NON, NON)): (True, '{#Up}'),
-    ((NON, NON), (NON, LOW, NON, NON)): (True, '{#Down}'),
-    # Move word 
-    ((NON, UPP), (UPP, NON, NON, NON)): (True, M('b')),
-    ((NON, UPP), (LOW, NON, NON, NON)): (True, M('f')),
+    ((NON, NON), (UPP, NON, NON, NON)): (True, '{#Up}'),
+    ((NON, NON), (LOW, NON, NON, NON)): (True, '{#Down}'),
+    ((NON, NON), (NON, UPP, NON, NON)): (True, '{#Left}'),
+    ((NON, NON), (NON, LOW, NON, NON)): (True, '{#Right}'),
+    # Move word
+    ((NON, UPP), (NON, UPP, NON, NON)): (True, M('b')),
+    ((NON, UPP), (NON, LOW, NON, NON)): (True, M('f')),
     # Move page
-    ((NON, UPP), (NON, UPP, NON, NON)): (True, from_emacs_command('scroll-down-command')),
-    ((NON, UPP), (NON, LOW, NON, NON)): (True, from_emacs_command('scroll-up-command')),
+    ((NON, UPP), (UPP, NON, NON, NON)): (True, from_emacs_command('scroll-down-command')),
+    ((NON, UPP), (LOW, NON, NON, NON)): (True, from_emacs_command('scroll-up-command')),
     # All Way Moves
-    ((NON, LOW), (UPP, NON, NON, NON)): (False, C('a')),
-    ((NON, LOW), (LOW, NON, NON, NON)): (False, C('e')),
-    ((NON, LOW), (NON, UPP, NON, NON)): (False, M('Shift_L(comma)')),
-    ((NON, LOW), (NON, LOW, NON, NON)): (False, M('Shift_L(period)')),
+    ((NON, LOW), (NON, UPP, NON, NON)): (False, C('a')),
+    ((NON, LOW), (NON, LOW, NON, NON)): (False, C('e')),
+    ((NON, LOW), (UPP, NON, NON, NON)): (False, M('Shift_L(comma)')),
+    ((NON, LOW), (LOW, NON, NON, NON)): (False, M('Shift_L(period)')),
     # Find Forward
-    ((NON, NON), (UPP, NON, LOW, NON)): (False, C('s')),
+    ((NON, NON), (NON, UPP, LOW, NON)): (False, C('s')),
     # Find Backwards
-    ((NON, NON), (LOW, NON, LOW, NON)): (False, C('r')),
+    ((NON, NON), (NON, LOW, LOW, NON)): (False, C('r')),
     # Alt-tab
     ((NON, UPP), (NON, NON, NON, NON)): (False, '{#Alt_L(Tab)}'),
     # Search and replace
-    ((NON, NON), (UPP, NON, LOW, LOW)): (False, M('Shift_L(percent)')),
+    ((NON, NON), (NON, LOW, LOW, LOW)): (False, M('Shift_L(percent)')),
     
  }
 
